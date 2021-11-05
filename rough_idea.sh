0cfc9d4850e356ccd61550f119fbb08af77a86c9
Emma MacKenzie <s1734289@bioinfmsc5.bio.ed.ac.uk>
2021-11-05 15:57:37 +0000

    created first file, splitting assignment into steps

---
+++ rough_idea.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+### in unix?
+
+# get user to provide a taxanomic group and family of proteins
+# download dataset
+
+# if no dataset available (or user provides the wrong name) give an error
+
+# set a max size, or get user to specify how many sequences they want 
+
+
+### in python, or by calling python scripts.
+
+
+# check how similar sequences are, what are the number of conserved bases
+# d
+# potentially create a heat map, by number of conserved bases (using mad table of likelihhod of mutation))
+
+
+# PROSITE database. what is it? use to identify motifs
+
+# look up other types of EMBOSS analysis, 
+# potentially output GC content of each sequence.
+
+
+### add clear error messages for each step 
