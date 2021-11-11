bd53e3f95bc5a2b29060168fb71ab76d423527b0
Emma MacKenzie <s1734289@bioinfmsc5.bio.ed.ac.uk>
2021-11-11 15:35:34 +0000

    Planning semi complete, basic idea of how to approach each process, ready to start coding

--- get_prosite.sh
+++ get_prosite.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# run acommand via commandline to run a sequnece through PROSITE ie https://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?seq=P98073
+# run acommand via commandline to run a sequnece through PROSITE ie get  https://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?seq=P98073
 # can use sequence name to run it, which may be in the fasta file 
 # use command GET, with the url attached to it.
+
+# header from generated fasta is not valid for prosite  
+# use wget, then link, with the sequence included in the seq section. ie wget https://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?seq=MIWVAVIGDWFNLIFKWILFGHRPYWWVQETMIYPNQSSPCLEQFPITCETGPGSPSGHAMGSSCVWYVM
+# need to save output in a different format.
+
+# then grep for motifs, count up number of motifs and produce both a file showing what motifs occur on each sequence, and a bar plot of the most common.
--- motifs_prep.py
+++ motifs_prep.py
@@ -11,5 +11,5 @@
 # PROSITE is a big database where sequences can be given to it and it will return a list of matching domains. hopefully there is a function that will allow remote access
 
 
-# check for non DNA characters in the sequence, check if they match IUPAC ambiguity codes - Lecture 11 (hopefully PROSITE uses them). if not then remove.
-# convert to protein sequences
+# check for non protein characters in the sequence, check if they match IUPAC ambiguity codes - Lecture 11 (hopefully PROSITE uses them). if not then remove.
+
