efa6aa906b3b0ec3900826f046762e7df486c4c6
Emma MacKenzie <s1734289@bioinfmsc5.bio.ed.ac.uk>
2021-11-15 20:40:34 +0000

    make downloading of sequences general, in python and check the file has sequences in it

--- get_seq.py
+++ get_seq.py
@@ -6,13 +6,16 @@
 
 import os
 import sys
+import subprocess
+import shutil
 
 # section to read in variables 
 
 try:
-    os.rmdir('temp')
+    shutil.rmtree('temp')
 except OSError as e:
-    print("Error: %s : %s" % ('temp', e.strerror))
+	# it is likely that temp does not exist, so don't want error to break the pipeline 
+	print("Error: %s : %s" % ('temp', e.strerror))
 
 subgroup = input("please enter subgroup of interest:")
 
@@ -33,13 +36,36 @@ while(type(protein) != str or len(protein) < 1)       :
 
 # create a directory to save temporary files in, so it is tidy and users can access different stages.
 
-os.mkdir(temp)
+os.mkdir('temp')
+
+# big horrible to look at text that is basically trying to make:
+# esearch -db protein -query "$protein AND $subgroup [ORGN]" |efetch -format fasta > "$subgroup"_"$protein".fasta
+# by inserting the python variables
+
+esearch_command = "esearch -db protein -query \"" + protein + " AND " + subgroup + " [ORGN]\" |efetch -format fasta >  temp/" + subgroup + "_" + protein + ".fasta"
 
 # insert variables into query, maybe have it so the input fasta has a name nade up of $subgroup and $protein 
- 
-# esearch -db protein -query "$protein AND $subgroup [ORGN]" |efetch -format fasta > "$subgroup"_"$protein".fasta
 
+print("Searching for " + protein + " in " + subgroup )
+
+# run the command in python, check for errors
+#### FIX THIS, ERRORS getting through as esearch tries multiple times. maybe use quietly 
+
+try:
+	os.system(esearch_command)
+	subprocess.call(esearch_command, shell=True)
+except OSError as e:
+	# insert more descriptive error
+        print("Error: esearch returned error")
 
 # if no file is returned, or the file is empty, or an error is returned, or the first line of input.fasta is errors then exit script
- 
+
+# check file is empty
+
+input_file_name =  "temp/" + subgroup + "_" + protein + ".fasta" 
+
+if os.stat(input_file_name).st_size == 0:
+	print("No sequences found for " + protein + " in " + subgroup)
+else	:
+	print("Sequences downloaded, beginning processing") 
 # call other scripts
